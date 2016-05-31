7a251274057c3b3e6659682252efcf9e4e822181
Blue <blubluescarlet@gmail.com>
2016-05-31 22:18:00 +0700

    Animation load
    - Services menu --> load animation when appear
    - ng-repeat end event

--- DAS/services/services.js
+++ DAS/services/services.js
@@ -22,12 +22,33 @@ services.controller("ServicesCtrl", ["$scope", "$firebaseObject",
         var ref = new Firebase("https://das-shop.firebaseio.com/service");
         var obj = $firebaseObject(ref);
         obj.$bindTo($scope, "data").then(function () {
-            //$scope.show = true;
+            
         });
+
+
+        $scope.onEnd = function () {
+            //$(".ser-grid-list").index().css('-webkit-animation', 'bounceInDown 1s');
+            var item = $(".ser-grid-list");
+            for (var i = 1; i < item.length + 1; ++i) {
+                $(".ser-grid-list:nth-child(" + i + ")").css("animation-delay", (i - 0.3 * i) + "s");
+                //$(".ser-grid-list").index.css('-webkit-animation', 'bounceindown ' + (i + 1) + 's');
+            }
+        };
+        
     }
 ]);
 
 
+services.directive("repeatEnd", function () {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            if (scope.$last) {
+                scope.$eval(attrs.repeatEnd);
+            }
+        }
+    };
+});
 
 services.animation('.ser-grid-list', ['$animateCss', function ($animateCss) {
     return {
